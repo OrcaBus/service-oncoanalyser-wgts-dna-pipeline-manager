3733d3ac1d7f2a5a5cc7e94458c1de246b5ba65f
Alexis Lucattini <alexis.lucattini@umccr.org>
2026-03-30 18:30:37 +1100

    Refactor binary checks in WRU draft script (#62)
    * Refactor binary checks in WRU draft script
    
    docs/operation/SOP/PM.OWD.1/generate-WRU-draft.sh
    -----
    *   Refactor binary and version checks in `check_binaries` to `return 1` on failure, allowing for centralized error handling and printing usage.
    *   Ensure the generated lambda payload is always printed to stderr for review, even when not requiring confirmation.
    *   Correct argument parsing and `jq` command block indentation for improved script readability.
    *   Append missing trailing slashes to example S3 URI prefixes in script usage notes.
    
    * Address copilot suggestions

--- docs/operation/SOP/PM.OWD.1/generate-WRU-draft.sh
+++ docs/operation/SOP/PM.OWD.1/generate-WRU-draft.sh
@@ -120,8 +120,8 @@ bash generate-WRU-draft.sh tumor_library_id normal_library_id \\
 bash generate-WRU-draft.sh tumor_library_id normal_library_id \\
   --comment 'Redriving analysis after failure' \\
   --output-uri-prefix s3://project-bucket/analysis/oncoanalyser-wgts-dna/ \\
-  --logs-uri-prefix s3://project-bucket/logs/oncoanalyser-wgts-dna \\
-  --cache-uri-prefix s3://project-bucket/cache/oncoanalyser-wgts-dna \\
+  --logs-uri-prefix s3://project-bucket/logs/oncoanalyser-wgts-dna/ \\
+  --cache-uri-prefix s3://project-bucket/cache/oncoanalyser-wgts-dna/ \\
   --project-id project-uuid-1234-abcd
 
 "
@@ -130,19 +130,32 @@ bash generate-WRU-draft.sh tumor_library_id normal_library_id \\
 compare_script_version_to_repo(){
   : '
   Compare the version of this script to the version in the repo, and print a warning if they are different
+  If anywhere along the way fails, return unknown
   '
   repo_script_version="$( \
-    curl --silent --fail --location --show-error \
-      --url "https://raw.githubusercontent.com/${GITHUB_REPO}/refs/heads/main/${THIS_SCRIPT_PATH}" | \
     (
-      grep -m1 "SOP_VERSION" | \
-      cut -d'"' -f2
+      # Read the document from the main branch
+      curl --silent --fail --location --show-error \
+        --header "Accept: text/html" \
+        --url "https://raw.githubusercontent.com/${GITHUB_REPO}/refs/heads/main/${THIS_SCRIPT_PATH}" | \
+      ( \
+        # Read through the whole document to prevent curl erroring out
+        tac | tac \
+      ) | \
+      (
+        # Get the first occurrence with grep -m1 (SOP_VERSION="YYYY.MM.DD")
+        # Remove the SOP_VERSION= prefix ("YYYY.MM.DD")
+        # Remove quotes (YYYY.MM.DD)
+        grep -m1 "SOP_VERSION" | \
+        sed 's/^SOP_VERSION=//' | \
+        jq --raw-output
+      ) \
     ) || echo "unknown"
   )"
 
   if [[ "${SOP_VERSION}" != "${repo_script_version}" ]]; then
     echo_stderr "Warning: This script version (${SOP_VERSION}) is different from the version in the repo (${repo_script_version})."
-    echo_stderr "Warning: Consider refetching this script from https://github.com/${GITHUB_REPO}/blob/main/${THIS_SCRIPT_PATH}"
+    echo_stderr "         Consider refetching this script from https://github.com/${GITHUB_REPO}/blob/main/${THIS_SCRIPT_PATH}"
   fi
 }
 
@@ -153,7 +166,7 @@ check_binaries(){
   for binary in aws semver jq curl openssl awk; do
     if ! command -v "${binary}" > /dev/null 2>&1; then
       echo_stderr "Error: ${binary} is not installed. Please install ${binary} and try again. Exiting."
-      exit 1
+      return 1
     fi
   done
 
@@ -161,26 +174,26 @@ check_binaries(){
   jq_version="$(jq --version | cut -d'-' -f2)"
   if [[ "${jq_version}" =~ ^1.\d$ && ! "${jq_version}" == "1.7" ]]; then
     echo_stderr "Error: jq version 1.7 or higher is required. Please update jq and try again. Exiting."
-    exit 1
+    return 1
   fi
   # After version 1.7, jq changed their versioning to semver, so we can use semver to compare versions
   if [[ ! "$(semver compare "${jq_version}" "${MIN_REQUIREMENTS["jq"]}")" -ge 0 ]]; then
     echo_stderr "Error: jq version ${MIN_REQUIREMENTS["jq"]} or higher is required. Please update jq and try again. Exiting."
-    exit 1
+    return 1
   fi
 
   # Check aws cli version is 2.0.0 or higher, as we use the --cli-binary-format option which was added in 2.0.0
   aws_version="$(aws --version 2>&1 | awk '{print $1}' | cut -d'/' -f2)"
   if [[ ! "$(semver compare "${aws_version}" "${MIN_REQUIREMENTS["aws"]}")" -ge 0 ]]; then
     echo_stderr "Error: AWS CLI version ${MIN_REQUIREMENTS["aws"]} or higher is required. Please update AWS CLI and try again. Exiting."
-    exit 1
+    return 1
   fi
 
   # Check curl version is 7.76.0 or higher, as we use the --fail-with-body option which was added in 7.76.0
   curl_version="$(curl --version | head -n1 | awk '{print $2}')"
   if [[ ! "$(semver compare "${curl_version}" "${MIN_REQUIREMENTS["curl"]}")" -ge 0 ]]; then
     echo_stderr "Error: curl version ${MIN_REQUIREMENTS["curl"]} or higher is required. Please update curl and try again. Exiting."
-    exit 1
+    return 1
   fi
 }
 
@@ -397,81 +410,81 @@ while [[ $# -gt 0 ]]; do
       ;;
     # Output URI prefix
     -o|--output-uri-prefix)
-    OUTPUT_URI_PREFIX="$2"
-    shift 2
-    ;;
-  -o=*|--output-uri-prefix=*)
-    OUTPUT_URI_PREFIX="${1#*=}"
-    shift
-    ;;
-  # Log URI prefix
-  -l|--logs-uri-prefix)
-    LOGS_URI_PREFIX="$2"
-    shift 2
-    ;;
-  -l=*|--logs-uri-prefix=*)
-    LOGS_URI_PREFIX="${1#*=}"
-    shift
-    ;;
-  # Cache URI prefix
-  -t|--cache-uri-prefix)
-    CACHE_URI_PREFIX="$2"
-    shift 2
-    ;;
-  -t=*|--cache-uri-prefix=*)
-    CACHE_URI_PREFIX="${1#*=}"
-    shift
-    ;;
-  # Project ID
-  -p|--project-id)
-    PROJECT_ID="$2"
-    shift 2
-    ;;
-  -p=*|--project-id=*)
-    PROJECT_ID="${1#*=}"
-    shift
-    ;;
-  # Analysis Storage Size
-  -s|--analysis-storage-size)
-    ANALYSIS_STORAGE_SIZE="$2"
-    shift 2
-    ;;
-  -s=*|--analysis-storage-size=*)
-    ANALYSIS_STORAGE_SIZE="${1#*=}"
-    shift
-    ;;
-  # Save draft payload to file
-  --save-draft-payload)
-    SAVE_DRAFT_PAYLOAD="$2"
-    shift 2
-    ;;
-  --save-draft-payload=*)
-    SAVE_DRAFT_PAYLOAD="${1#*=}"
-    shift
-    ;;
-  # Workflow version
-  --workflow-version)
-    WORKFLOW_VERSION="$2"
-    shift 2
-    ;;
-  --workflow-version=*)
-    WORKFLOW_VERSION="${1#*=}"
-    shift
-    ;;
-  # Code version
-  --code-version)
-    CODE_VERSION="$2"
-    shift 2
-    ;;
-  --code-version=*)
-    CODE_VERSION="${1#*=}"
-    shift
-    ;;
-  # Positional arguments (library IDs)
-  *)
-    LIBRARY_ID_ARRAY+=("$1")
-    shift
-    ;;
+      OUTPUT_URI_PREFIX="$2"
+      shift 2
+      ;;
+    -o=*|--output-uri-prefix=*)
+      OUTPUT_URI_PREFIX="${1#*=}"
+      shift
+      ;;
+    # Log URI prefix
+    -l|--logs-uri-prefix)
+      LOGS_URI_PREFIX="$2"
+      shift 2
+      ;;
+    -l=*|--logs-uri-prefix=*)
+      LOGS_URI_PREFIX="${1#*=}"
+      shift
+      ;;
+    # Cache URI prefix
+    -t|--cache-uri-prefix)
+      CACHE_URI_PREFIX="$2"
+      shift 2
+      ;;
+    -t=*|--cache-uri-prefix=*)
+      CACHE_URI_PREFIX="${1#*=}"
+      shift
+      ;;
+    # Project ID
+    -p|--project-id)
+      PROJECT_ID="$2"
+      shift 2
+      ;;
+    -p=*|--project-id=*)
+      PROJECT_ID="${1#*=}"
+      shift
+      ;;
+    # Analysis Storage Size
+    -s|--analysis-storage-size)
+      ANALYSIS_STORAGE_SIZE="$2"
+      shift 2
+      ;;
+    -s=*|--analysis-storage-size=*)
+      ANALYSIS_STORAGE_SIZE="${1#*=}"
+      shift
+      ;;
+    # Save draft payload to file
+    --save-draft-payload)
+      SAVE_DRAFT_PAYLOAD="$2"
+      shift 2
+      ;;
+    --save-draft-payload=*)
+      SAVE_DRAFT_PAYLOAD="${1#*=}"
+      shift
+      ;;
+    # Workflow version
+    --workflow-version)
+      WORKFLOW_VERSION="$2"
+      shift 2
+      ;;
+    --workflow-version=*)
+      WORKFLOW_VERSION="${1#*=}"
+      shift
+      ;;
+    # Code version
+    --code-version)
+      CODE_VERSION="$2"
+      shift 2
+      ;;
+    --code-version=*)
+      CODE_VERSION="${1#*=}"
+      shift
+      ;;
+    # Positional arguments (library IDs)
+    *)
+      LIBRARY_ID_ARRAY+=("$1")
+      shift
+      ;;
   esac
 done
 
@@ -529,7 +542,11 @@ declare -A MIN_REQUIREMENTS=(
   ["aws"]="2.0.0"    # Because what are you doing still on V1?
   ["curl"]="7.76.0"  # For --fail-with-body option
 )
-check_binaries
+if ! check_binaries; then
+  echo_stderr "Error: One or more required binaries are not installed. Please install the required binaries and try again. Exiting."
+  print_usage
+  exit 1
+fi
 
 # AWS Account ID by prefix
 declare -A PREFIX_BY_AWS_ACCOUNT_ID=(
@@ -602,24 +619,24 @@ fi
 echo_stderr "Generating engine parameters"
 engine_parameters=$( \
   jq --null-input --raw-output --compact-output \
-  --arg outputUriPrefix "${OUTPUT_URI_PREFIX}" \
-  --arg logsUriPrefix "${LOGS_URI_PREFIX}" \
-  --arg cacheUriPrefix "${CACHE_URI_PREFIX}" \
-  --arg projectId "${PROJECT_ID}" \
-  --arg portalRunId "${portal_run_id}" \
-  --arg analysisStorageSize "${ANALYSIS_STORAGE_SIZE}" \
-  '
-    # Get the engine parameters
-    {
-      "outputUri": ( if $outputUriPrefix != "" then ($outputUriPrefix + $portalRunId + "/") else "" end ),
-      "logsUri": ( if $logsUriPrefix != "" then ($logsUriPrefix + $portalRunId + "/") else "" end ),
-      "cacheUri": ( if $cacheUriPrefix != "" then ($cacheUriPrefix + $portalRunId + "/") else "" end ),
-      "projectId": $projectId,
-      "analysisStorageSize": $analysisStorageSize
-    } |
-    # Remove empty values
-    with_entries(select(.value != ""))
-  ' \
+    --arg outputUriPrefix "${OUTPUT_URI_PREFIX}" \
+    --arg logsUriPrefix "${LOGS_URI_PREFIX}" \
+    --arg cacheUriPrefix "${CACHE_URI_PREFIX}" \
+    --arg projectId "${PROJECT_ID}" \
+    --arg portalRunId "${portal_run_id}" \
+    --arg analysisStorageSize "${ANALYSIS_STORAGE_SIZE}" \
+    '
+      # Get the engine parameters
+      {
+        "outputUri": ( if $outputUriPrefix != "" then ($outputUriPrefix + $portalRunId + "/") else "" end ),
+        "logsUri": ( if $logsUriPrefix != "" then ($logsUriPrefix + $portalRunId + "/") else "" end ),
+        "cacheUri": ( if $cacheUriPrefix != "" then ($cacheUriPrefix + $portalRunId + "/") else "" end ),
+        "projectId": $projectId,
+        "analysisStorageSize": $analysisStorageSize
+      } |
+      # Remove empty values
+      with_entries(select(.value != ""))
+    ' \
 )
 
 # Generate the event
@@ -651,10 +668,9 @@ lambda_payload="$( \
 )"
 
 # Confirm before pushing the event
+echo_stderr "Send the following payload to the lambda object:"
+jq --raw-output <<< "${lambda_payload}" 1>&2
 if [[ "${FORCE}" == "false" ]]; then
-    echo_stderr "Send the following payload to the lambda object:"
-    jq --raw-output <<< "${lambda_payload}" 1>&2
-
     read -r -p 'Confirm to push this event to EventBridge? (y/n): ' confirm_push
     if [[ ! "${confirm_push}" =~ ^[Yy]$ ]]; then
       echo_stderr "Aborting event push."
